c80a425c1e2be4c2899d400d4a2e721338559e45
JoseLuisGL <89991775+JoseLuisGL@users.noreply.github.com>
2024-04-21 19:11:44 -0700

    Get y Post

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+var users = [
+  {
+    id : 1,
+    name: "Jhon",
+    email: "<EMAIL>"
+  },
+  {
+    id: 2,
+    name: "Jane",
+    email: "<EMAIL>"
+  }
+];
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(users);
+});
+
+router.post('/', function(req, res, next) {
+  var user = req.body;
+  users.push(user);
+  res.json(user);
+});
+
+
+
+
+module.exports = router;
