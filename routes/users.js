1e8fcd16acc90dc895f67b8280d28a60a28ba249
JoseLuisGL <89991775+JoseLuisGL@users.noreply.github.com>
2024-04-21 19:33:19 -0700

    Se agrega PUT

--- routes/users.js
+++ routes/users.js
@@ -20,11 +20,27 @@ router.get('/', function(req, res, next) {
   res.json(users);
 });
 
+/* POST users listing. */
 router.post('/', function(req, res, next) {
   var user = req.body;
   users.push(user);
   res.json(user);
 });
+
+/* PUT users listing. */
+router.put('/:id', function(req, res, next) {
+  var userId = req.params.id;
+  var updatedUser = req.body;
+
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].id == userId) {
+      users[i] = updatedUser;
+      return res.json(updatedUser);
+    }
+  }
+  res.status(404).json({ message: 'User not found' });
+});
+
 
 
 
